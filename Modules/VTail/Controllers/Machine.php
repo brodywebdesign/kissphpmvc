e0699942d817900cd01673879cc9d4db808b1d20
Michael Dennis <michaeladennis@yahoo.com>
2018-03-07 18:31:56 -0700

    adding some basic logic

---
+++ Modules/VTail/Controllers/Machine.php
@@ -0,0 +1,23 @@
+<?php
+namespace Modules\VTail\Controllers;
+use \Library\Controller;
+class Machine extends Controller
+{
+    public function status()
+    {
+        //check the param
+        //look up the machine
+        //choose if post or get
+        //verify the update, or return the machine status
+        //add the status if the machine exists.
+    }
+
+    public function stock()
+    {
+        //check the param
+        //look up the machine
+        //choose if post or get
+        //verify the update, or return the machine stock
+        //add the stock update if the machine exists.
+    }
+}
